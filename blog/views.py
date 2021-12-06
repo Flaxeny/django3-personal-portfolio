5fa3e7d783e32d22a8d7db152403dd399d54b6c3
Roman Skorokhodov <romskor11@gmail.com>
2021-12-07 00:15:53 +0300

    Editing blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from .models import BlogSpot
 
 
 def all_blogs(request):
-    return render(request, 'blog/all_blogs.html')
+    blogs = BlogSpot.objects.order_by('-date')[:5]
+    return render(request, 'blog/all_blogs.html', {'blogs': blogs})
--- personal_portfolio_project/urls.py
+++ personal_portfolio_project/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from portfolio import views
 
 urlpatterns = [
-    path('admin', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('startpage', views.startpage, name='home'),
     path('blog', include('blog.urls')),
 ]
